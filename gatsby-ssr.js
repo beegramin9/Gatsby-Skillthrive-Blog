d0167ff152fa97991333b9ac6bc80360f0b95f2d
wontae <beegramin9@gmail.com>
2021-08-03 20:30:58 +0900

    added global style

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,23 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import theme from './src/themes/theme';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body, html {
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.them.colors.light};
+    }
+`
 
 export const wrapRootElement = ({ element }) => (
     <ThemeProvider theme={theme}>
-        {element}
+        <GlobalStyle/>
+            {element} {/* => will be the final React application */}
     </ThemeProvider>
 );
