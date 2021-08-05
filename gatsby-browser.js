9d416fb84b618c7694556cbc0a50ac58f9d1a0fd
wontae <beegramin9@gmail.com>
2021-08-05 21:21:03 +0900

    added MDX style

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,13 @@
+//! gatsby-ssr.js와 gatsby-browser.js는 똑같아야 한다.
 import React from "react";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import theme from './src/themes/theme';
+
 import { MDXProvider } from '@mdx-js/react';
-import { Table } from './src/components';
+import { Table, Code } from './src/components';
+
+import { preToCodeBlock } from "mdx-utils";
+import "./language-tabs.css";
 
 const GlobalStyle = createGlobalStyle`
     * {
@@ -19,6 +24,15 @@ const GlobalStyle = createGlobalStyle`
 
 const components = {
     table: Table,
+    pre: preProps => {
+        const props = preToCodeBlock(preProps);
+        // if there's a codeString and some props, we passed the test
+        if (props) {
+            return <Code {...props}/>
+        }
+        return <pre {...preProps}/>
+    },
+    wrapper: ({children}) => <>{children}</>
 }
 
 export const wrapRootElement = ({ element }) => (
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,13 @@
+//! gatsby-ssr.js와 gatsby-browser.js는 똑같아야 한다.
 import React from "react";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import theme from './src/themes/theme';
+
+import { MDXProvider } from '@mdx-js/react';
+import { Table, Code } from './src/components';
+
+import { preToCodeBlock } from "mdx-utils";
+import "./language-tabs.css";
 
 const GlobalStyle = createGlobalStyle`
     * {
@@ -11,13 +18,28 @@ const GlobalStyle = createGlobalStyle`
     body, html {
         font-family: ${props => props.theme.fonts.main};
         height: 100%;
-        background-color: ${props => props.them.colors.light};
+        background-color: ${props => props.theme.colors.light};
     }
 `
 
+const components = {
+    table: Table,
+    pre: preProps => {
+        const props = preToCodeBlock(preProps);
+        // if there's a codeString and some props, we passed the test
+        if (props) {
+            return <Code {...props}/>
+        }
+        return <pre {...preProps}/>
+    },
+    wrapper: ({children}) => <>{children}</>
+}
+
 export const wrapRootElement = ({ element }) => (
-    <ThemeProvider theme={theme}>
-        <GlobalStyle/>
-            {element} {/* => will be the final React application */}
-    </ThemeProvider>
+    <MDXProvider components={components}>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle/>
+                {element} {/* => will be the final React application */}
+        </ThemeProvider>
+    </MDXProvider>
 );
